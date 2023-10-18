24af0fe8e6c12bef593b4a0e547817ccfab418e5
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-18 07:45:44 +0700

    ok test git

--- Sources/GoiAPI1/GoiAPI1.swift
+++ Sources/GoiAPI1/GoiAPI1.swift
@@ -17,4 +17,17 @@ public struct GoiAPI1 {
         let x = 2
         print("Hello toi tu trong package ne", (i + x))
     }
+    
+    public func thuChayFor(n:Int){
+        let languages = ["Swift", "Java", "Go", "JavaScript"]
+
+        for language in languages {
+              print(language)
+        }
+        
+        // iterate from i = 1 to i = 3
+        for i in 1...n {
+            print(i)
+        }
+    }
 }
