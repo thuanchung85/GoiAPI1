db1b9b1142f84971ed1ca6136121fcdb05fd4e6a
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-19 13:08:31 +0700

    save

--- Sources/GoiAPI1/GoiAPI1.swift
+++ Sources/GoiAPI1/GoiAPI1.swift
@@ -10,18 +10,32 @@ public struct GoiAPI1 {
     public init() {
     }
     
+    //===hàm chạy khởi tạo địa chỉ ví 0x..... trên iPhone===//
     public func hamChayThu_tao_keystore(passwordString:String) -> String{
         do {
          let keystore = try EthereumKeystoreV3.init(password: passwordString)
-            let string1 = keystore?.addresses
-            
-            let returnString = string1?.first?.address
-            return returnString ?? "no data"
+         
+            return keystore?.addresses?.first?.address ?? "no data"
          } catch {
          print(error.localizedDescription)
          }
        return "no data"
     }
     
+    //===hàm chạy khởi tạo 12 từ Mnemonic Phrase trên iPhone===//
+    public func hamChayThu_tao_12Words(passwordString:String) -> [String]{
+        do {
+            let mnemonic = try BIP39.generateMnemonics(bitsOfEntropy: 256)!
+            
+            let keystore = try BIP32Keystore(mnemonics: mnemonic,
+                                              password: passwordString,
+                                              mnemonicsPassword: passwordString)
+            
+            return [mnemonic,keystore?.addresses?.first?.address ?? "no data"]
+         } catch {
+         print(error.localizedDescription)
+         }
+        return ["no data"]
+    }
     
 }
