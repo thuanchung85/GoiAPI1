07c613fcec1283841d391648da4c30e0a666cd51
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-19 14:12:03 +0700

    save

--- Sources/GoiAPI1/GoiAPI1.swift
+++ Sources/GoiAPI1/GoiAPI1.swift
@@ -38,4 +38,22 @@ public struct GoiAPI1 {
         return ["no data"]
     }
     
-}
+    //==hàm chạy lấy số dư của một địa chỉ EthereumAddress===//
+    public func hamChayThu_get_BalanceEthereumAddress(address:String) async -> [String]{
+        do {
+            
+            let InfuraMainnetWeb3 = try await Web3.InfuraMainnetWeb3(accessToken: "b9ce386fa2b3415eb3df790155d24675")
+            print("InfuraMainnetWeb3: ", InfuraMainnetWeb3)
+            let balanceETH = try await InfuraMainnetWeb3.eth.getBalance(for: EthereumAddress("0xb794f5ea0ba39494ce839613fffba74279579268")!)
+                print("balanceETH: ", balanceETH)
+            
+            return [String(balanceETH)]
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        return ["no data"]
+    }
+    
+    
+}//end struct
