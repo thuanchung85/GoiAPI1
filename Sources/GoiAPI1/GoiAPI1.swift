0da86c65ab5160d2388ab2d6b8cc59bc7fa13292
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-19 12:56:23 +0700

    save

--- Sources/GoiAPI1/GoiAPI1.swift
+++ Sources/GoiAPI1/GoiAPI1.swift
@@ -3,13 +3,6 @@ import web3swift
 import Web3Core
 
 
-func json(from object:Any) -> String? {
-    guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
-        return nil
-    }
-    return String(data: data, encoding: String.Encoding.utf8)
-}
-
 
 public struct GoiAPI1 {
     public private(set) var text = "Hello, This is GoiAPI1!"
@@ -17,14 +10,12 @@ public struct GoiAPI1 {
     public init() {
     }
     
-    public func hamChayThu_tao_keystore() -> String{
+    public func hamChayThu_tao_keystore(passwordString:String) -> String{
         do {
-         let keystore = try EthereumKeystoreV3.init(password: "12345")
+         let keystore = try EthereumKeystoreV3.init(password: passwordString)
             let string1 = keystore?.addresses
-            let json_string1 = json(from:string1 as Any)
             
-            
-            let returnString = json_string1
+            let returnString = string1?.first?.address
             return returnString ?? "no data"
          } catch {
          print(error.localizedDescription)
