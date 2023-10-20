ed3854cf964004fa28d32129571b259e04b8e92e
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-20 07:42:02 +0700

    save

--- Sources/GoiAPI1/GoiAPI1.swift
+++ Sources/GoiAPI1/GoiAPI1.swift
@@ -4,26 +4,26 @@ import Web3Core
 
 
 
-public struct GoiAPI1 {
-    public private(set) var text = "Hello, This is GoiAPI1!"
-
-    public init() {
+public class GoiAPI1 {
+    var bip32keystore:BIP32Keystore?
+    var keystoremanager:KeystoreManager?
+    var InfuraMainnetWeb3: Web3?
+    
+    let userDirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]+"/keystore/"
+    
+    public init() async {
+        do{
+            InfuraMainnetWeb3 = try await Web3.InfuraMainnetWeb3(accessToken: "b9ce386fa2b3415eb3df790155d24675")
+            keystoremanager =  KeystoreManager.managerForPath(userDirPath, scanForHDwallets: true, suffix: "json")
+            print("CONNECTED -> INFURA")
+        } catch {
+        print(error.localizedDescription)
+        }
     }
     
     //===hàm chạy khởi tạo địa chỉ ví 0x..... trên iPhone===//
-    public func hamChayThu_tao_keystore(passwordString:String) -> String{
-        do {
-         let keystore = try EthereumKeystoreV3.init(password: passwordString)
-         
-            return keystore?.addresses?.first?.address ?? "no data"
-         } catch {
-         print(error.localizedDescription)
-         }
-       return "no data"
-    }
-    
     //===hàm chạy khởi tạo 12 từ Mnemonic Phrase trên iPhone===//
-    public func hamChayThu_tao_12Words(passwordString:String) -> [String]{
+    public func hamChayThu_tao_12Words(passwordString:String) async -> [String]{
         do {
             let mnemonic = try BIP39.generateMnemonics(bitsOfEntropy: 256)!
             
@@ -31,7 +31,11 @@ public struct GoiAPI1 {
                                               password: passwordString,
                                               mnemonicsPassword: passwordString)
             
-            return [mnemonic,keystore?.addresses?.first?.address ?? "no data"]
+            InfuraMainnetWeb3!.addKeystoreManager(keystoremanager)
+            self.bip32keystore = self.keystoremanager?.bip32keystores[0]
+            let address = self.bip32keystore?.addresses?.first?.address
+            
+            return [mnemonic,address ?? "no data"]
          } catch {
          print(error.localizedDescription)
          }
@@ -63,7 +67,7 @@ public struct GoiAPI1 {
             let transactionCount_pending =  try await InfuraMainnetWeb3.eth.getTransactionCount(for: EthereumAddress(address)!,onBlock: .pending)
             let transactionCount_earliest =  try await InfuraMainnetWeb3.eth.getTransactionCount(for: EthereumAddress(address)!,onBlock: .earliest)
             let transactionCount_latest =  try await InfuraMainnetWeb3.eth.getTransactionCount(for: EthereumAddress(address)!,onBlock: .latest)
-           
+          
             print("contract.transactionCount_pending: " , transactionCount_pending)
             print("contract.transactionCount_earliest: " , transactionCount_earliest)
             print("contract.transactionCount_latest: " , transactionCount_latest)
