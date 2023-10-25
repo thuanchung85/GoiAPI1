8f94f2f828d464b7551daad76a95ad2112919c39
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-25 15:56:12 +0700

    save

--- Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
+++ Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
@@ -52,7 +52,7 @@ public struct ReInputMnemonicWordsView: View {
                 LazyVGrid(columns: columns,alignment: .center, spacing: 10) {
                     ForEach(data12Words, id: \.self) { item in
                         Text(item.components(separatedBy: ": ").last ?? " ")
-                            .frame(width: 100)
+                            .frame(width: 80)
                             .font(.body)
                             .foregroundColor(.white)
                             .padding()
