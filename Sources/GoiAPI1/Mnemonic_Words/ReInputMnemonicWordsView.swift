c0217fafd13dbeb6edcdab5a6507ef8b8e567dd2
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-25 15:37:04 +0700

    save

--- Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
+++ Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
@@ -9,6 +9,9 @@ public struct ReInputMnemonicWordsView: View {
     
     @Binding var isShowReInput12SeedsView:Bool
     
+    @State var seedsTextString = ["words1","words2","words3","words4",
+                                  "words5","words6","words7","words8",
+                                  "words9","words10","words11","words12"]
     //===INIT===///
     public init(isShowReInput12SeedsView:Binding<Bool>) {
         self._isShowReInput12SeedsView = isShowReInput12SeedsView
@@ -26,7 +29,12 @@ public struct ReInputMnemonicWordsView: View {
             
             //12 từ trong khung
             ScrollView {
-               
+               Text(seedsTextString.joined(separator: " "))
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .padding(10)
             }
             .frame(maxHeight: 500)
             
