48cb2b4757b2617d89ab45d1f1f4ffdd6af49d2b
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-27 07:26:27 +0700

    save

--- Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
+++ Sources/GoiAPI1/Mnemonic_Words/ReInputMnemonicWordsView.swift
@@ -38,157 +38,159 @@ public struct ReInputMnemonicWordsView: View {
     
     public var body: some View{
         VStack(alignment: .center) {
-            
-            Text("ReEnter Your 12 words seed phrase").font(.title)
-                .padding(10)
-            Text("Please tap your 12 words follow previous order. ")
-                .font(.footnote)
-                .padding(.bottom,5)
-            
-            //12 từ trong khung
-            Text(seedsTextString.joined(separator: " "))
+            //show page tập input 12 từ cho user
+            if(showQRCodePage == false){
+                Text("ReEnter Your 12 words seed phrase").font(.title)
+                    .padding(10)
+                Text("Please tap your 12 words follow previous order. ")
+                    .font(.footnote)
+                    .padding(.bottom,5)
+                
+                //12 từ trong khung
+                Text(seedsTextString.joined(separator: " "))
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .background(Color.gray)
-                        .padding(10)
-                        .cornerRadius(5)
-                        .lineSpacing(10)
-            //nút delete 1 từ
-            HStack(){
-                Spacer()
-                if(isInput12SeedsDone == false){
-                    Button(action: {
-                        if(currentIndexSeed > 0){
-                            currentIndexSeed -= 1
-                            seedsTextString[currentIndexSeed] = String(currentIndexSeed + 1) + ":..."
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .padding(10)
+                    .cornerRadius(5)
+                    .lineSpacing(10)
+                //nút delete 1 từ
+                HStack(){
+                    Spacer()
+                    if(isInput12SeedsDone == false){
+                        Button(action: {
+                            if(currentIndexSeed > 0){
+                                currentIndexSeed -= 1
+                                seedsTextString[currentIndexSeed] = String(currentIndexSeed + 1) + ":..."
+                            }
+                        }) {
+                            VStack {
+                                Text("Delete")
+                            }
+                            .padding(5)
+                            .accentColor(Color(.red))
                         }
-                    }) {
-                        VStack {
-                            Text("Delete")
-                        }
-                        .padding(5)
-                        .accentColor(Color(.red))
+                        
                     }
-                    
-                }
-            }//end HStack
-            
-           Divider()
-            
-            //nếu vẫn chưa ok re check 12 words thì chưa show next button
-            if(finalReCheckResult == false){
-                if(isInput12SeedsDone == false){
-                    //12 button seeds
-                    ScrollView {
-                        LazyVGrid(columns: columns,alignment: .center, spacing: 10) {
-                            ForEach(data12Words.shuffled(), id: \.self) { item in
-                                let s = item.components(separatedBy: ": ").last ?? " "
-                                if(seedsTextString.contains(s) == false){
-                                    Text(s)
-                                        .frame(width: 80)
-                                        .font(.body)
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .background(Color.blue)
-                                        .cornerRadius(5)
-                                        .scaledToFill()
-                                        .minimumScaleFactor(0.5)
-                                        .lineLimit(1)
-                                        .onTapGesture {
-                                            print("seed word: \(currentIndexSeed)", s)
-                                            seedsTextString[currentIndexSeed] = s
-                                            currentIndexSeed += 1
-                                            
-                                            //nếu là từ cuối cùng thì check kết quả luôn
-                                            if(currentIndexSeed >= 12){
-                                                print("ket qua re check : ", seedsTextString)
-                                                let array_Data12Words = data12Words.map { i in
-                                                   return i.components(separatedBy: ": ").last ?? " "
+                }//end HStack
+                
+                Divider()
+                
+                //nếu vẫn chưa ok re check 12 words thì chưa show next button
+                if(finalReCheckResult == false){
+                    if(isInput12SeedsDone == false){
+                        //12 button seeds
+                        ScrollView {
+                            LazyVGrid(columns: columns,alignment: .center, spacing: 10) {
+                                ForEach(data12Words.shuffled(), id: \.self) { item in
+                                    let s = item.components(separatedBy: ": ").last ?? " "
+                                    if(seedsTextString.contains(s) == false){
+                                        Text(s)
+                                            .frame(width: 80)
+                                            .font(.body)
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color.blue)
+                                            .cornerRadius(5)
+                                            .scaledToFill()
+                                            .minimumScaleFactor(0.5)
+                                            .lineLimit(1)
+                                            .onTapGesture {
+                                                print("seed word: \(currentIndexSeed)", s)
+                                                seedsTextString[currentIndexSeed] = s
+                                                currentIndexSeed += 1
+                                                
+                                                //nếu là từ cuối cùng thì check kết quả luôn
+                                                if(currentIndexSeed >= 12){
+                                                    print("ket qua re check : ", seedsTextString)
+                                                    let array_Data12Words = data12Words.map { i in
+                                                        return i.components(separatedBy: ": ").last ?? " "
+                                                    }
+                                                    print("data12Words : ", array_Data12Words)
+                                                    if(seedsTextString == array_Data12Words){
+                                                        self.finalReCheckResult = true
+                                                    }
+                                                    isInput12SeedsDone = true
+                                                    currentIndexSeed = 0
                                                 }
-                                                print("data12Words : ", array_Data12Words)
-                                                if(seedsTextString == array_Data12Words){
-                                                    self.finalReCheckResult = true
-                                                }
-                                                isInput12SeedsDone = true
-                                                currentIndexSeed = 0
                                             }
-                                        }
-                                }
-                            }//end for each
-                        }//end LazyVGrid
-                        .padding(.horizontal)
-                    }//end ScrollView
-                    .frame(maxHeight: 700)
+                                    }
+                                }//end for each
+                            }//end LazyVGrid
+                            .padding(.horizontal)
+                        }//end ScrollView
+                        .frame(maxHeight: 700)
+                        
+                    }
+                    else{
+                        Text("SORRY, YOU GOT THE WRONG ORDER, PLEASE TRY AGAIN.").foregroundColor(Color.red)
+                    }
+                    //nút back để user làm lại
+                    //nut skip nếu user thôi không làm nữa
+                    HStack(alignment: .center){
+                        Spacer()
+                        Button(action: {
+                            self.isShowReInput12SeedsView = false
+                            
+                        }) {
+                            VStack {
+                                Text("TRY AGAIN")
+                            }
+                            .padding()
+                            .accentColor(Color(.systemBlue))
+                            .cornerRadius(4.0)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
+                            )
+                        }
+                        Spacer()
+                        Button(action: {
+                            showQRCodePage = true
+                        }) {
+                            VStack {
+                                Text("SKIP").foregroundColor(Color.red)
+                            }
+                            .padding()
+                            .accentColor(Color(.red))
+                            .cornerRadius(4.0)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
+                            )
+                        }
+                        Spacer()
+                    }//end HStack
+                    
+                    
                     
                 }
+                
+                //nếu PASS re check 12 words thì show next button
                 else{
-                    Text("SORRY, YOU GOT THE WRONG ORDER, PLEASE TRY AGAIN.").foregroundColor(Color.red)
+                    Text("WELL DONE,YOU WERE CORRECTED ALL WORDS").foregroundColor(Color.green)
+                    
+                    //nút NEXT
+                    HStack(alignment: .center){
+                        Spacer()
+                        Button(action: {
+                            showQRCodePage = true
+                            
+                        }) {
+                            VStack {
+                                Text("NEXT")
+                            }
+                            .padding()
+                            .accentColor(Color(.systemBlue))
+                            .cornerRadius(4.0)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
+                            )
+                        }
+                        Spacer()
+                    }//end HStack
                 }
-                //nút back để user làm lại
-                //nut skip nếu user thôi không làm nữa
-                HStack(alignment: .center){
-                    Spacer()
-                    Button(action: {
-                        self.isShowReInput12SeedsView = false
-                        
-                    }) {
-                        VStack {
-                            Text("TRY AGAIN")
-                        }
-                        .padding()
-                        .accentColor(Color(.systemBlue))
-                        .cornerRadius(4.0)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
-                        )
-                    }
-                    Spacer()
-                    Button(action: {
-                        showQRCodePage = true
-                    }) {
-                        VStack {
-                            Text("SKIP").foregroundColor(Color.red)
-                        }
-                        .padding()
-                        .accentColor(Color(.red))
-                        .cornerRadius(4.0)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
-                        )
-                    }
-                    Spacer()
-                }//end HStack
-                
-               
                 
             }
-            
-            //nếu PASS re check 12 words thì show next button
-            else{
-                Text("WELL DONE,YOU WERE CORRECTED ALL WORDS").foregroundColor(Color.green)
-                
-                //nút NEXT
-                HStack(alignment: .center){
-                    Spacer()
-                    Button(action: {
-                        showQRCodePage = true
-                        
-                    }) {
-                        VStack {
-                            Text("NEXT")
-                        }
-                        .padding()
-                        .accentColor(Color(.systemBlue))
-                        .cornerRadius(4.0)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
-                        )
-                    }
-                    Spacer()
-                }//end HStack
-            }
-            
             
             //show page QRcode 12 từ cho user copy
             if(showQRCodePage == true){
