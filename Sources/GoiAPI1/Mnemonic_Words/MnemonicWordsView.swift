3f8ad1e08d924286fccdd5591be35a97c7559ebd
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-30 14:45:26 +0700

    save

--- Sources/GoiAPI1/Mnemonic_Words/MnemonicWordsView.swift
+++ Sources/GoiAPI1/Mnemonic_Words/MnemonicWordsView.swift
@@ -34,68 +34,14 @@ public struct MnemonicWordsView: View {
     }
     //====BODY====///
     public var body: some View{
-        if(isStillLoading12Word == true){
-            LoadingView(addressWallet: $addressWallet, data12Words: $data12Words, isStillLoading12Word: $isStillLoading12Word,
-                        walletName: $walletName, PIN_Number: $PIN_Number, isShowing:  $isStillLoading12Word)
-            {
-                //12 seed words View
-                VStack(alignment: .center) {
-                    
-                    Text("Your 12 words seed phrase")
-                        .font(.custom("Arial ", size: 20))
-                        .padding(10)
-                    Text("Below are 12 recovery words connected to your wallet. Please store it securely and never share it with anyone.")
-                        .font(.footnote)
-                        .padding(.bottom,10)
-                    
-                    //12 từ trong khung
-                    ScrollView {
-                        LazyVGrid(columns: columns,alignment: .center, spacing: 10) {
-                            ForEach(Array(data12Words.enumerated()), id: \.offset) { index,item in
-                                Text("\(index + 1) : \(item)")
-                                    .frame(width: 130)
-                                    .font(.body)
-                                    .foregroundColor(.blue)
-                                    .padding()
-                                    .border(.blue)
-                                    .cornerRadius(5)
-                                
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                    .frame(maxHeight: 500)
-                    
-                    //show address ví của user
-                    Text("Account Address:\n" + addressWallet).font(.body).padding(.horizontal)
-                    
-                    //nút next
-                    HStack(alignment: .center){
-                        Spacer()
-                        Button(action: {
-                            
-                            
-                        }) {
-                            VStack {
-                                Text("NEXT")
-                            }
-                            .padding()
-                            .accentColor(Color(.systemBlue))
-                            .cornerRadius(4.0)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 4).stroke(Color(.systemBlue), lineWidth: 2)
-                            )
-                        }
-                        Spacer()
-                    }//end VStack
-                    
-                    
-                }
-            }
+        if(isStillLoading12Word == true)
+        {
+            
             
         }
         //nếu load 12 từ xong thì show ra 12 từ đó, và chuẫn bị view "cho user nhập lại 12 từ"
-        else{
+        if(isStillLoading12Word == false)
+        {
             //nếu isShowReInput12SeedsView = false thì cứ show 12 từ ra bình thường.
             if(isShowReInput12SeedsView == false){
                 //show 12 seed words View
