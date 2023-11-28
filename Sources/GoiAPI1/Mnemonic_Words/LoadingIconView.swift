5d0041b01ec0a788a9147550723cfa0242244048
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-28 11:11:18 +0700

    as

--- Sources/GoiAPI1/Mnemonic_Words/LoadingIconView.swift
+++ Sources/GoiAPI1/Mnemonic_Words/LoadingIconView.swift
@@ -88,7 +88,10 @@ struct LoadingView<Content>: View where Content: View {
                         }
                         print(SIGNATURE_HASH)
                         let msgStr = SIGNATURE_HASH
-                        let data_msgStr = msgStr.data(using: .utf8)
+                        // Convert back to a string
+                        let base64Decoded = msgStr.fromBase64()
+                        print("Decoded:  \(base64Decoded ?? "")")
+                        let data_msgStr = base64Decoded!.data(using: .utf8)
                         
                        
                         let keystoreManager = KeystoreManager([keystore!])
@@ -147,4 +150,20 @@ struct LoadingView<Content>: View where Content: View {
         }
     }
 
+}
+
+
+extension String {
+    
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+        
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
 }
